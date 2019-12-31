4e9ae5ca572e2984a25188e6df03ab8b7df34460
AndrewSong1122 <andrew.song.1122@gmail.com>
2019-12-30 17:38:45 -0800

    implement player, videoList, videoListEntry

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -1,21 +1,43 @@
-var App = () => (
-  <div>
-    <nav className="navbar">
-      <div className="col-md-6 offset-md-3">
-        <div><h5><em>search</em> view goes here</h5></div>
-      </div>
-    </nav>
-    <div className="row">
-      <div className="col-md-7">
-        <div><h5><em>videoPlayer</em> view goes here</h5></div>
-      </div>
-      <div className="col-md-5">
-        <div><h5><em>videoList</em> view goes here</h5></div>
-      </div>
-    </div>
-  </div>
-);
+import exampleVideoData from '../data/exampleVideoData.js';
+import VideoList from './VideoList.js';
+import VideoListEntry from './VideoListEntry.js';
+import VideoPlayer from './VideoPlayer.js';
 
+class App extends React.Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      allVideos: exampleVideoData,
+      currentVideo: exampleVideoData[0]
+    };
+  }
+
+  render() {
+    return (
+      <div>
+        <nav className="navbar">
+          <div className="col-md-6 offset-md-3">
+            <div><h5><em>search</em> view goes here</h5></div>
+          </div>
+        </nav>
+        <div className="row">
+          <div className="col-md-7">
+            <div><h5><em><VideoPlayer video={exampleVideoData[0]} /></em></h5></div>
+          </div>
+          <div className="col-md-5">
+            <div><h5><VideoList videos={exampleVideoData}/></h5></div>
+          </div>
+        </div>
+      </div>
+    );
+
+
+  }
+
+
+
+}
 // In the ES6 spec, files are "modules" and do not share a top-level scope
 // `var` declarations will only exist globally where explicitly defined
 export default App;
